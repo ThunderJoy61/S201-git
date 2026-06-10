c777e3533c9258735b7ca05f847ec550ecbd0a3f
m_nguyen <minh-tam1.nguyen@etu.u-pec.fr>
2026-06-10 18:53:08 +0200

    Changement de la page d'accueil avec un avant gout

--- controllers/accueil.py
+++ controllers/accueil.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template
+from services.ameli_api import AmeliAPI
 from models.db import Session
 from models.dimensions import Region, ProfessionSante
 
 bp_accueil = Blueprint("accueil", __name__)
+
+api = AmeliAPI()
 
 @bp_accueil.route("/")
 def index():
@@ -12,11 +15,15 @@ def index():
     try:
         regions = session.query(Region).order_by(Region.libelle).all()
         professions = session.query(ProfessionSante).order_by(ProfessionSante.libelle).all()
+        preview = api.get_effectifs("Infirmiers", "999", 2024, 2024, "99")
+        evolution = api.get_evolution_effectifs("Infirmiers", "999", "99")
 
         return render_template(
             "accueil.html",
             regions=regions,
-            professions=professions
+            professions=professions,
+            preview=preview,
+            evolution=evolution
         )
 
     finally:
