1b63bc78caf74c292eb8e39bd2d76a2742263565
ThunderJoy61 <crepieuxadrien@gmail.com>
2026-06-12 09:53:07 +0200

    modif de accueil

--- controllers/accueil.py
+++ controllers/accueil.py
@@ -7,6 +7,7 @@ bp_accueil = Blueprint("accueil", __name__)
 
 api = AmeliAPI()
 
+
 @bp_accueil.route("/")
 def index():
     """Page d'accueil."""
@@ -15,15 +16,19 @@ def index():
     try:
         regions = session.query(Region).order_by(Region.libelle).all()
         professions = session.query(ProfessionSante).order_by(ProfessionSante.libelle).all()
-        preview = api.get_effectifs("Infirmiers", "999", 2024, 2024, "99")
+
+        preview = api.get_effectifs("Infirmiers", "999", 2020, 2024, "99")
         evolution = api.get_evolution_effectifs("Infirmiers", "999", "99")
+
+        preview_item = preview[-1] if preview else None
 
         return render_template(
             "accueil.html",
             regions=regions,
             professions=professions,
-            preview=preview,
-            evolution=evolution
+            preview_item=preview_item,
+            evolution=evolution,
+            api_error=api.last_error,
         )
 
     finally:
